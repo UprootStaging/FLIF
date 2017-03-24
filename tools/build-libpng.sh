1f54e5a82308e6d81cf795067f2a931201717db8
hrj <harshad.rj@gmail.com>
2017-03-24 11:43:44 +0000

    travis: removed buggy command

--- tools/build-libpng.sh
+++ tools/build-libpng.sh
@@ -2,7 +2,7 @@ set -ex
 
 mkdir ~/build-libpng
 cd ~/build-libpng
-travis_retry wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
+wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
 tar xf libpng-1.6.28.tar.xz
 cd libpng-1.6.28/
 ./configure || cat config.log
