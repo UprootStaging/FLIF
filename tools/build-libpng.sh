6068a1d2cd32e558b5d25b60e5f7e9c6282bc46b
hrj <harshad.rj@gmail.com>
2017-03-24 10:19:35 +0000

    travis: tar xf

--- tools/build-libpng.sh
+++ tools/build-libpng.sh
@@ -3,6 +3,7 @@ set -ex
 mkdir ~/build-libpng
 cd ~/build-libpng
 wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
+tar xf libpng-1.6.28.tar.xz
 cd libpng-1.6.28/
 ./configure
 make install
