bd7ae5be78bfdb0beb628e2d4e9c62ac88d68850
hrj <harshad.rj@gmail.com>
2017-03-24 11:40:58 +0000

    travis: retry download and print log

--- tools/build-libpng.sh
+++ tools/build-libpng.sh
@@ -2,8 +2,8 @@ set -ex
 
 mkdir ~/build-libpng
 cd ~/build-libpng
-wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
+travis_retry wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
 tar xf libpng-1.6.28.tar.xz
 cd libpng-1.6.28/
-./configure
+./configure || cat config.log
 make install
