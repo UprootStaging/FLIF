ff7c6cdc7860b17cf421fbb53f1d9a1d654089bc
hrj <harshad.rj@gmail.com>
2017-04-04 11:22:51 +0000

    travis: fix directory stack

--- tools/build-libpng.sh
+++ tools/build-libpng.sh
@@ -3,12 +3,11 @@
 set -ex
 
 mkdir ~/build-libpng
-cd ~/build-libpng
+pushd ~/build-libpng
 wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
 tar xf libpng-1.6.28.tar.xz
 cd libpng-1.6.28/
 ./configure || cat config.log
 make install
 
-popd
 popd
